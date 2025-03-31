15c67611c15653e1edf995f8ef30897bd4065022
urtanto <vladanikanov19@gmail.com>
2025-03-31 07:02:29 +0300

    added migration

--- database/models/audio.py
+++ database/models/audio.py
@@ -11,6 +11,6 @@ class AudioFile(SqlAlchemyBase):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
     filename = Column(String, nullable=False)
-    user_id = Column(String, ForeignKey("users.id"))
+    user_id = Column(UUID, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="audio_files")
