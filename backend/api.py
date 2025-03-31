e3417fb35cdb78e8437d0a76c8189f5f6d79b1e2
urtanto <vladanikanov19@gmail.com>
2025-03-31 16:42:04 +0300

    delete user

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,11 @@
+import uuid
 from typing import Optional
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy import select
 
-from backend.auth import get_user
+from backend.auth import get_user, get_admin
 from database import Database
 from database.models import User
 
@@ -45,7 +46,7 @@ async def patch_me(update: UserUpdate, user: User = Depends(get_user)):
 
 
 @api_router.get("/users")
-async def patch_me(user: User = Depends(get_user)):
+async def patch_me(_: User = Depends(get_user)):
     async with await Database().get_session() as session:
         async with session.begin():
             users: list[User] = list((
@@ -63,3 +64,20 @@ async def patch_me(user: User = Depends(get_user)):
             } for user in users
         ]
     }
+
+@api_router.delete("/user/{user_id}")
+async def delete_user(user_id: str, _: User = Depends(get_admin)):
+    async with await Database().get_session() as session:
+        async with session.begin():
+            user_to_delete: User = (
+                await session.execute(
+                    select(User).where(User.id == uuid.UUID(user_id))
+                )
+            ).unique().scalar_one_or_none()
+
+            if not user_to_delete:
+                return {"message": "User not found"}
+
+            await session.delete(user_to_delete)
+            await session.commit()
+    return {"message": "User deleted"}
