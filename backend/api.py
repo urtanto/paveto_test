3e9bb7a696116c4cae4edfe18d919e64923d67cf
urtanto <vladanikanov19@gmail.com>
2025-03-31 16:38:27 +0300

    get users

--- backend/api.py
+++ backend/api.py
@@ -1,27 +1,19 @@
-import datetime
-import os
-import uuid
 from typing import Optional
 
-import aiohttp
-import jwt
-from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy import select
-from starlette.responses import RedirectResponse
 
-import database
+from backend.auth import get_user
 from database import Database
 from database.models import User
-from backend.auth import get_user, get_admin
 
 api_router = APIRouter()
+
 
 class UserUpdate(BaseModel):
     name: Optional[str] = None
     email: Optional[str] = None
-
 
 
 @api_router.get("/me")
@@ -33,16 +25,11 @@ async def get_me(user: User = Depends(get_user)):
         "email": user.email
     }
 
+
 @api_router.patch("/me")
 async def patch_me(update: UserUpdate, user: User = Depends(get_user)):
     async with await Database().get_session() as session:
         async with session.begin():
-            # db_user: User = (
-            #     await session.execute(
-            #         select(User).where(User.id == user.id)
-            #     )
-            # ).unique().scalar_one_or_none()
-
             if update.name:
                 user.name = update.name
             if update.email:
@@ -54,4 +41,25 @@ async def patch_me(update: UserUpdate, user: User = Depends(get_user)):
         "yandex_id": user.yandex_id,
         "name": user.name,
         "email": user.email
+    }
+
+
+@api_router.get("/users")
+async def patch_me(user: User = Depends(get_user)):
+    async with await Database().get_session() as session:
+        async with session.begin():
+            users: list[User] = list((
+                await session.execute(
+                    select(User)
+                )
+            ).scalars().all())
+    return {
+        "users": [
+            {
+                "id": str(user.id),
+                "yandex_id": user.yandex_id,
+                "name": user.name,
+                "email": user.email
+            } for user in users
+        ]
     }
