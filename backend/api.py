bc917b4545638541bdae85d037e5ab712fdab7dd
urtanto <vladanikanov19@gmail.com>
2025-03-31 16:33:43 +0300

    fix bugs

--- backend/api.py
+++ backend/api.py
@@ -37,21 +37,21 @@ async def get_me(user: User = Depends(get_user)):
 async def patch_me(update: UserUpdate, user: User = Depends(get_user)):
     async with await Database().get_session() as session:
         async with session.begin():
-            db_user: User = (
-                await session.execute(
-                    select(User).where(User.id == user.id)
-                )
-            ).unique().scalar_one_or_none()
+            # db_user: User = (
+            #     await session.execute(
+            #         select(User).where(User.id == user.id)
+            #     )
+            # ).unique().scalar_one_or_none()
 
             if update.name:
-                db_user.name = update.username
+                user.name = update.name
             if update.email:
-                db_user.email = update.email
+                user.email = update.email
 
             await session.commit()
     return {
-        "id": str(db_user.id),
-        "yandex_id": db_user.yandex_id,
-        "name": db_user.name,
-        "email": db_user.email
+        "id": str(user.id),
+        "yandex_id": user.yandex_id,
+        "name": user.name,
+        "email": user.email
     }
